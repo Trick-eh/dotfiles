55b9ad406622d5b120a25b890d02d947ee90e040
Trick-eh <juanpelegrinac@gmail.com>
2025-12-05 19:02:07 -0300

    preview capabilities for nvim improved via plugins

--- .config/nvim/lua/trickk/plugins/omni-preview.lua
+++ .config/nvim/lua/trickk/plugins/omni-preview.lua
@@ -1,25 +1,17 @@
 return {
 	"sylvanfranklin/omni-preview.nvim",
 	dependencies = {
-		{ "toppair/peek.nvim", lazy = true, build = "deno task --quiet build:fast" }, -- markdown
-		{ "chomosuke/typst-preview.nvim", lazy = true }, -- typst
-		{ "hat0uma/csvview.nvim", lazy = true }, -- csv
+		{ "chomosuke/typst-preview.nvim", lazy = true }, -- typst previewer
+		{ "hat0uma/csvview.nvim", lazy = true }, -- csv previewer
+		{ "toppair/peek.nvim", lazy = true, build = "deno task --quiet build:fast" }, -- markdown previewer
+		{ "hat0uma/csvview.nvim", lazy = true },
 	},
 	config = function()
 		require("omni-preview").setup()
-		require("typst-preview").setup({
-			dependencies_bin = {
-				["tinymist"] = "/home/trickk/.local/share/nvim/mason/bin/tinymist",
-				["websocat"] = nil,
-			},
-		})
-
 		require("peek").setup({ app = "browser" })
-		require("csvview").setup({
-			view = {
-				display_mode = "border",
-			},
-		})
-		vim.keymap.set("n", "<leader>p", ":OmniPreview start<CR>", { silent = true })
 	end,
+	keys = {
+		{ "<leader>ps", "<cmd>OmniPreview start<CR>", desc = "OmniPreview Start" },
+		{ "<leader>pf", "<cmd>OmniPreview stop<CR>", desc = "OmniPreview Stop" },
+	},
 }
