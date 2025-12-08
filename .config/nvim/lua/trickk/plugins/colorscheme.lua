dc027fcf2b6b1571bc9860afe38e232a5bbe8ba1
Trick-eh <juanpelegrinac@gmail.com>
2025-12-08 16:34:12 -0300

    swaync 'riced'

--- .config/nvim/lua/trickk/plugins/colorscheme.lua
+++ .config/nvim/lua/trickk/plugins/colorscheme.lua
@@ -1,22 +1,22 @@
-local THEME = os.getenv("THEME")
+local THEME = "miasma"
 
 if THEME == "rose-pine" then
-    return {
-        "rose-pine/neovim",
-        priority = 1000,
-        name = "rose-pine",
-        config = function()
-            require("rose-pine").setup()
-            vim.cmd("colorscheme rose-pine")
-        end
-    }
+	return {
+		"rose-pine/neovim",
+		priority = 1000,
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup()
+			vim.cmd("colorscheme rose-pine")
+		end,
+	}
 elseif THEME == "miasma" then
-    return {
-        "xero/miasma.nvim",
-        priority = 1000,
-        name = "miasma",
-        config = function()
-            vim.cmd("colorscheme miasma")
-        end
-    }
+	return {
+		"xero/miasma.nvim",
+		priority = 1000,
+		name = "miasma",
+		config = function()
+			vim.cmd("colorscheme miasma")
+		end,
+	}
 end
