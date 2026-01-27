ed9c928c5e9f3426333d97d69bb8ef8eb45a729f
Trick-eh <juanpelegrinac@gmail.com>
2026-01-27 20:38:02 -0300

    FIXED: cliphist no longer stores passwords copied via the wofi-passmenu script until reboot

--- .scripts/wofi-passmenu.sh
+++ .scripts/wofi-passmenu.sh
@@ -3,5 +3,6 @@ last_selection = $(wl-paste)
 wofi-pass -c -i -s
 if [ last_selection != $(wl-paste) ]; then
     notify-send "Password copied to clipboard" -t 3000 -e
-    sleep 10 && printf "" | wl-copy
+    sleep 10 && cliphist delete-query $(cliphist decode $(cliphist list | head -n 1 | cut -f 1))
+    printf "" | wl-copy
 fi
